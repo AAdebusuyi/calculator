7447bf16b8787fdfee1efbc8f1e75ebcad89276f
AAdebusuyi <adeoladonald7@gmail.com>
2023-09-02 05:51:20 +0100

    basic functions

---
+++ script.js
@@ -0,0 +1,37 @@
+let num1;
+let num2;
+let operator;
+
+function add(...numbers) {
+  return numbers.reduce((total, value) => (total += value), 0);
+}
+
+function subtract(...numbers) {
+  return numbers.reduce((total, value) => (total -= value));
+}
+
+function multiply(...numbers) {
+  return numbers.reduce((total, value) => (total *= value));
+}
+
+function divide(...numbers) {
+  return numbers.reduce((total, value) => (total /= value));
+}
+
+function operate(num1, num2, operator) {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      if (num1 === 0 || num2 === 0) return null;
+      else return divide(num1, num2);
+    default:
+      return null;
+  }
+}
