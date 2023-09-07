b85794f6daad0e797902fbd711554107cb25b6f7
AAdebusuyi <adeoladonald7@gmail.com>
2023-09-07 14:21:11 +0100

    checking clicked button's type

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 const buttons = document.querySelector(".buttons-container");
+const screen = document.querySelector(".screen");
 
-//check button type
+//checks the type of button clicked
 buttons.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
     const button = e.target;
     const action = button.dataset.action;
+    const buttonValue = button.textContent;
+    const screenValue = screen.textContent;
     if (!action) {
-      console.log("number button.");
+      // checks for when number buttons are clicked
+      if (screenValue === "0") {
+        screen.textContent = buttonValue;
+      } else {
+        screen.textContent += buttonValue;
+      }
     }
     if (
+      //checks for when operator buttons are clicked
       action === "add" ||
       action === "subtract" ||
       action === "multiply" ||
       action === "divide"
     ) {
-      console.log("operator button.");
+      button.classList.add("is-depressed");
+      setTimeout(() => {
+        button.classList.remove("is-depressed");
+      }, 75);
     }
     if (action === "decimal") {
-      console.log("decimal button");
+      // checks decimal button...
+      screen.textContent += ".";
     }
     if (action === "equal") {
+      // checks equals to button...
       console.log("equals button");
     }
     if (action === "clear") {
+      // checks clear button...
       console.log("clear button");
     }
     if (action === "delete") {
+      //checks delete button...
       console.log("delete button");
     }
   }
