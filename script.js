db9f20d965e56dc81695d9e332e5a59ef84e7dcd
AAdebusuyi <adeoladonald7@gmail.com>
2023-09-07 10:40:22 +0100

    checking button type

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-const buttons = document.querySelectorAll(".buttons-container");
+const buttons = document.querySelector(".buttons-container");
 
-keys.addEventListener("click", (e) => {
+//check button type
+buttons.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
-    // do something
+    const button = e.target;
+    const action = button.dataset.action;
+    if (!action) {
+      console.log("number button.");
+    }
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      console.log("operator button.");
+    }
+    if (action === "decimal") {
+      console.log("decimal button");
+    }
+    if (action === "equal") {
+      console.log("equals button");
+    }
+    if (action === "clear") {
+      console.log("clear button");
+    }
+    if (action === "delete") {
+      console.log("delete button");
+    }
   }
 });
